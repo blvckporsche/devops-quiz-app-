8152ce13e67fbfae25145456af735a9a24518dc9
Moazzam <60301940@udst.edu.qa>
2026-02-05 14:27:41 +0300

    Populate topic dropdown from questions

--- app.js
+++ app.js
@@ -6,3 +6,22 @@ fetch('data/questions.json')
     questions = data;
     console.log("Questions loaded", questions.length);
   });
+function loadTopics() {
+    const topics = [...new Set(questions.map(q => q.topic))];
+    const select = document.getElementById('topicSelect');
+  
+    topics.forEach(topic => {
+      const option = document.createElement('option');
+      option.value = topic;
+      option.textContent = topic;
+      select.appendChild(option);
+    });
+  }
+  
+fetch('data/questions.json')
+    .then(res => res.json())
+    .then(data => {
+      questions = data;
+      loadTopics();
+    });
+  
